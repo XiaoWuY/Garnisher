a2a8823e8508f2e77f224edbe68689e10ffbc858
unknown <you@example.com>
2020-08-19 17:15:50 +0800

    第二次提交

---
+++ GarnisherOne/SuitPersonOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garnisher.GarnisherOne
+{
+    // 穿西装的人
+    public class SuitPersonOne : PersonOne
+    {
+        public void Show()
+        {
+            base.Show();
+            Console.Write("西装  ");
+        }
+
+    }
+}
