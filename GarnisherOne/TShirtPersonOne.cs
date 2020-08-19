c65cf0e54267af35e52cc527ad529045abc43483
unknown <you@example.com>
2020-08-19 17:16:23 +0800

    第二次提交

---
+++ GarnisherOne/TShirtPersonOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garnisher.GarnisherOne
+{
+    // 穿大t恤的人
+    public class TShirtPersonOne: PersonOne
+    {
+        public void Show()
+        {
+            base.Show();
+            Console.Write("大t恤  ");
+        }
+    }
+}
